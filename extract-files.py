d0ca317d6c57e04c1aac918841f78e59cda9689d
LuK1337 <priv.luk@gmail.com>
2024-11-06 11:56:59 +0100

    sm8635-common: Use common lib_fixups
    Change-Id: I862e547f3375184729eb1a909bb7d6f676d45dfe
    Signed-off-by: hasan6034 <h.basri3460@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -11,12 +11,8 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
-    lib_fixup_remove_arch_suffix,
-    lib_fixup_vendorcompat,
+    lib_fixups,
     lib_fixups_user_type,
-    libs_clang_rt_ubsan,
-    libs_proto_3_9_1,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -38,8 +34,7 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 
 
 lib_fixups: lib_fixups_user_type = {
-    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
-    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.diaghal@1.0',
